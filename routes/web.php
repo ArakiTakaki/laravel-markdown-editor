dd8ed567c6e7eaeae7fb32d8e9631a5c3d03ba5c
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-03 23:17:52 +0900

    一旦退避

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +12,31 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * 全タスクの表示
+ */
+Route::get('/task', 'TaskController@get');
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task', function(Request $request){
+    $validator = $request->validate([
+        'name' => 'required|max:255'
+    ]);
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * 既存タスクの削除
+ */
+Route::delete('/task/{id}', function($id){
+
 });
