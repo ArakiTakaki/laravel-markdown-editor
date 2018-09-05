5d27f58d3fc87a433fb98ac684cf1eb4c514c6a9
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-05 12:15:00 +0900

    ４０４ページの作成

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">404 Not found</div>
+
+                <div class="card-body">
+                  ページが存在しません
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
