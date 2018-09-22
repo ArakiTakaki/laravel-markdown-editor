45d1fd48c4c58cc3461c864a8f0313a39038c025
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-22 15:39:49 +0900

    laradockの生成用f

--- laradock.sh
+++ laradock.sh
@@ -1,4 +1,8 @@
 #!bin/bash
+PROJECT_DIR="PROJECT_DIR=${PWD}/public"
 cp laradock-env-example ./laradock/.env
 cd laradock
 docker-compose up -d mysql apache2 workspace phpmyadmin
+cd ..
+cd frontend
+echo $PROJECT_DIR >> .env
