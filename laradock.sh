9776279ba3ac5c328964802b8706b8611ced5d60
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-22 22:24:58 +0900

    追記

--- laradock.sh
+++ laradock.sh
@@ -3,6 +3,7 @@ PROJECT_DIR="PROJECT_DIR=${PWD}/public"
 cp laradock-env-example ./laradock/.env
 cd laradock
 docker-compose up -d mysql apache2 workspace phpmyadmin
+php artisan key:generate
 cd ..
 cd frontend
 echo $PROJECT_DIR >> .env
