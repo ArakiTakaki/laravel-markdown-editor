a4a48a16c8e5caab7a81ac02fe11ac24513c7a15
ArakiTakaki <araki_takaki@yahoo.co.jp>
2018-09-23 00:10:01 +0900

    gitignore調整

--- setup.sh
+++ setup.sh
@@ -4,12 +4,17 @@ PROJECT_DIR="PROJECT_DIR=${PWD}/public"
 
 git clone https://github.com/laradock/laradock
 cp env-example .env
+
+# Laradockの設定
 cp laradock-env-example ./laradock/.env
 cd laradock
-
 docker-compose up -d mysql apache2 workspace phpmyadmin
 php artisan key:generate
 cd ..
+# Laradockの設定
+
+# nodeの設定
 cd frontend
 echo $PROJECT_DIR >> .env
 npm install
+# nodeの設定
